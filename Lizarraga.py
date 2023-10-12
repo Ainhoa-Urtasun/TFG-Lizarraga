d10c32e19e1baea8e9632e0d14c01f83664df61b
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-12 21:03:52 +0200

    Update Lizarraga.py

--- Lizarraga.py
+++ Lizarraga.py
@@ -36,6 +36,8 @@ mydata1 = mydata[mydata.time=='2022']
 table = mydata.pivot(index='ADMIN',columns='time',values=['percentage'])
 table.columns = table.columns.droplevel(level=0)
 table = table.reset_index()
+table = table.drop('time',axis=1)
+table.rename(columns={'ADMIN':'GEO'},inplace=True)
 
 mydata1 = mydata1.merge(europe,on='ADMIN',how='right')
 mydata1 = geopandas.GeoDataFrame(mydata1,geometry='geometry')
